701240e6adae3ac32d524253f9627d26f0b3e0c8
Anthony COUDENE <anthony.coudene@technidata-web.com>
2025-08-05 17:08:10 +0200

    Just create a dedicated project to .Net Framework 4.6.1 using Environment variable. An enhancement of core components should be applied.

---
+++ ConfigurationUsing/ConfigurationUsingApp.net461/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConfigurationUsingApp.net461
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      string envVar = Environment.GetEnvironmentVariable("AppConfig__Message");
+
+      if (string.IsNullOrEmpty(envVar))
+      {
+        Console.WriteLine("La variable d'environnement 'AppConfig__Message' n'est pas définie.");
+      }
+      else
+      {
+        Console.WriteLine($"Valeur de AppConfig__Message : {envVar}");
+      }
+
+    }
+  }
+}

d07646341138c29a1837d8b31d97b8700c983440
Anthony COUDENE <anthony.coudene@technidata-web.com>
2025-08-05 20:43:54 +0200

    First step for obfuscation in .Net 9.0 and try Obfuscar

---
+++ Obsucation/ObsucationApp/MySensitiveData.cs
@@ -0,0 +1,11 @@
+﻿namespace ObsucationApp;
+
+public class MySensitiveData
+{
+  public const string _mySecretKey = "MySuperSecret";
+
+  public string PrintSecret()
+  {
+    return $"The secret key is: {_mySecretKey}";
+  }
+}
---
+++ Obsucation/ObsucationApp/Program.cs
@@ -0,0 +1,7 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using ObsucationApp;
+
+
+var myData = new MySensitiveData();
+Console.WriteLine($"Your secret: {myData.PrintSecret()}");

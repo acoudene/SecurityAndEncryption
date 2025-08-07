94812b6d11796e90b59e1a44ff255eba642825b1
Anthony COUDENE <anthony.coudene@technidata-web.com>
2025-08-07 20:52:59 +0200

    Just add text

--- PasswordHashing/PasswordHashingApp/Program.cs
+++ PasswordHashing/PasswordHashingApp/Program.cs
@@ -9,5 +9,5 @@ for (int i = 0; i < 10; i++)
   (string hash, string salt) = PasswordHelper.HashPassword(myPassword, Argon2Hasher.Hash);
   Console.WriteLine($"Argon2 - Password: {myPassword}, Hash: {hash}, Salt: {salt}");
   bool verified = PasswordHelper.VerifyPassword(myPassword, hash, salt, Argon2Hasher.Hash);
-  Console.WriteLine($"Argon2 - Password verified: {verified}");
+  Console.WriteLine($"Argon2 - Password verified result: {verified}");
 }

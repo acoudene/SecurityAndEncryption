46d31d61e63dfdd1023081acec6a48e1f0ad020b
Anthony COUDENE <anthony.coudene@technidata-web.com>
2025-08-09 13:32:25 +0200

    Add RSA cryptography and signature approach

---
+++ RSA/CryptoRsaApp/Program.cs
@@ -0,0 +1,42 @@
+﻿
+// Génération d'une paire de clés RSA
+using System.Security.Cryptography;
+using System.Text;
+
+using RSA rsa = RSA.Create(2048);
+
+// Key exportation
+string publicKey = Convert.ToBase64String(rsa.ExportRSAPublicKey());
+string privateKey = Convert.ToBase64String(rsa.ExportRSAPrivateKey());
+
+Console.WriteLine("Public key: " + publicKey);
+Console.WriteLine("Private key: " + privateKey);
+
+// Message to encrypt
+string message = "Hi Anthony!";
+byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+for (int i = 0; i < 3; i++)
+{
+  // Cryptography with the public key
+  byte[] encryptedBytes = rsa.Encrypt(messageBytes, RSAEncryptionPadding.OaepSHA256);
+  Console.WriteLine("Crypted message: " + Convert.ToBase64String(encryptedBytes));
+
+  // With the private key, decrypting the message
+  byte[] decryptedBytes = rsa.Decrypt(encryptedBytes, RSAEncryptionPadding.OaepSHA256);
+  string decryptedMessage = Encoding.UTF8.GetString(decryptedBytes);
+  Console.WriteLine("Decrypted message: " + decryptedMessage);
+}
+//Fonctionnement simplifié de OAEP :
+//- Message original(M) est préparé.
+//- Un masque est généré à partir d'une source aléatoire (appelée seed).
+//- Ce masque est appliqué au message via une fonction de hachage (souvent SHA-1 ou SHA-256).
+//- Le message masqué est ensuite combiné avec le seed masqué.
+//- Le tout est chiffré avec RSA.
+//Ce processus garantit que :
+//- Le même message chiffré deux fois donnera des résultats différents (grâce au seed aléatoire).
+//- Le message est protégé contre les attaques structurelles.
+//✅ Avantages de OAEP :
+//- Sécurité renforcée contre les attaques adaptatives.
+//- Compatibilité avec les standards modernes (PKCS#1).
+//- Utilisation courante dans les bibliothèques comme .NET, Java, OpenSSL.
---
+++ RSA/SignRsaApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+using RSA rsa = RSA.Create(2048);
+
+// Message to sign
+string message = "Message to sign";
+byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+// Sign the message with the private key
+byte[] signature = rsa.SignData(messageBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+Console.WriteLine("Signature: " + Convert.ToBase64String(signature));
+
+// Check the signature with the public key
+bool isValid = rsa.VerifyData(messageBytes, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+Console.WriteLine("Is signature valid? " + isValid);

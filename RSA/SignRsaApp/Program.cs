f0a213253580053d9c9e0a9be477d444dccbbd21
Anthony COUDENE <anthony.coudene@technidata-web.com>
2025-08-09 14:05:46 +0200

    RSA simple data signature and SHA-256+RSA hash signature

--- RSA/SignRsaApp/Program.cs
+++ RSA/SignRsaApp/Program.cs
@@ -1,16 +1,41 @@
-﻿using System.Security.Cryptography;
+﻿using System.Collections;
+using System.Security.Cryptography;
 using System.Text;
 
-using RSA rsa = RSA.Create(2048);
+// RSA data signature example
+{
+  using RSA rsa = RSA.Create(2048);
 
-// Message to sign
-string message = "Message to sign";
-byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+  // Message to sign
+  string message = "Message to sign";
+  byte[] messageBytes = Encoding.UTF8.GetBytes(message);
 
-// Sign the message with the private key
-byte[] signature = rsa.SignData(messageBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-Console.WriteLine("Signature: " + Convert.ToBase64String(signature));
+  // Sign the message with the private key
+  byte[] signature = rsa.SignData(messageBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+  Console.WriteLine($"Signature: {Convert.ToBase64String(signature)}");
 
-// Check the signature with the public key
-bool isValid = rsa.VerifyData(messageBytes, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-Console.WriteLine("Is signature valid? " + isValid);
+  // Check the signature with the public key
+  bool isValid = rsa.VerifyData(messageBytes, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+  Console.WriteLine($"Is signature valid? {isValid}");
+}
+
+// With SHA-256 and RSA hash signature  
+{
+  using RSA rsa = RSA.Create(2048);
+
+  // Message to sign
+  string message = "Message to sign";
+  byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+  // Step1: Hash the message using SHA-256
+  using SHA256 sha256 = SHA256.Create();
+  byte[] hash = sha256.ComputeHash(messageBytes);
+
+  // Step 2: Sign the hash with the RSA private key
+  byte[] signature = rsa.SignHash(hash, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+  Console.WriteLine($"Signature: {Convert.ToBase64String(signature)}");
+
+  // Step 3: Verify the signature using the RSA public key
+  bool isValid = rsa.VerifyHash(hash, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+  Console.WriteLine($"Is signature valid? {isValid}");  
+}

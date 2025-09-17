e80fc76a4b24d9e537156900aef2f379098a0fd1
Anthony COUDENE <anthony.coudene@technidata-web.com>
2025-09-17 12:04:20 +0200

    Add jaeger for observability

--- CertificateManagement/CertificateManagement.AppHost/AppHost.cs
+++ CertificateManagement/CertificateManagement.AppHost/AppHost.cs
@@ -1,5 +1,12 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
+/// Use Jaeger
+var jaeger = builder.AddContainer("jaeger", "jaegertracing/all-in-one")
+    .WithEndpoint("http", endpoint => { endpoint.Port = 16686; endpoint.TargetPort = 16686; endpoint.UriScheme = "http"; })     // Interface web Jaeger
+    .WithEndpoint("gRPC", endpoint => { endpoint.Port = 4317; endpoint.TargetPort = 4317; endpoint.UriScheme = "http"; })       // OTLP gRPC
+    .WithEndpoint("http-oltp", endpoint => { endpoint.Port = 4318; endpoint.TargetPort = 4318; endpoint.UriScheme = "http"; })  // OTLP HTTP
+ ;
+
 // Add parameters for username and password
 var username = builder.AddParameter("username", "admin");
 var password = builder.AddParameter("password", "admin", secret: true);
@@ -7,7 +14,7 @@ var password = builder.AddParameter("password", "admin", secret: true);
 var keycloak = builder.AddKeycloak("keycloak", 9090, username, password)
   .WithArgs(
   "--tracing-enabled=true", // Enable tracing for monitoring, export by default to http://localhost:4317 in gRPC
-//  "--tracing-endpoint=http://jaeger:4317", // 👈 Ajout de l'endpoint correct
+  "--tracing-endpoint=http://jaeger:4317", // 👈 Ajout de l'endpoint correct
   "--metrics-enabled=true", // Enable metrics for monitoring
   "--event-metrics-user-enabled=true",
   "--event-metrics-user-events=login,logout",
@@ -17,7 +24,7 @@ var keycloak = builder.AddKeycloak("keycloak", 9090, username, password)
   )
   .WithRealmImport("./Realms")
   //.WithLifetime(ContainerLifetime.Persistent)
-  //.WaitFor(jaeger) // Configure Keycloak with a persistent lifetime, useful to avoid long startup times on each run
+  .WaitFor(jaeger) // Configure Keycloak with a persistent lifetime, useful to avoid long startup times on each run
   ;
 
 builder.AddProject<Projects.AuthenticationCertificateApp>("authenticationcertificateapp")

779d6c99200cd976ea99679be9b653a33d584110
Anthony COUDENE <anthony.coudene@technidata-web.com>
2025-10-08 14:59:20 +0200

    Add XSS value to correct

---
+++ TSSLearning/SqlInjection/_01_SqlInjectionApp/AccountManagement/Api/UserDetailsEndpointsExtensions.Safe.cs
@@ -0,0 +1,39 @@
+﻿namespace _01_SqlInjectionApp.AccountManagement.Api;
+
+using _01_SqlInjectionApp.Users.Application;
+using System.Net.Mime;
+using System.Text;
+
+public static class UserDetailsEndpointsSafeExtensions
+{
+  public static void MapUserDetailsSafeEndpoints(this IEndpointRouteBuilder app)
+  {
+    // TODO
+    // Field with: 
+    // <td>Ah ah ah <img src="https://www.powertrafic.fr/wp-content/uploads/2023/04/image-ia-exemple.png" onload="alert('virus !')"/></td>
+    
+    app.MapGet("/api/users/details/{name}", async (IUsersRepository repo, string name) =>
+    {
+      StringBuilder sb = new StringBuilder();
+      sb.AppendLine("<html>");
+      sb.AppendLine("<body>");
+
+      var users = await repo.GetByPatternAsync(name);
+      foreach (var user in users)
+      {        
+        sb
+        .Append("<h1>")
+        .Append($"{user.FirstName} {user.Name}")
+        .AppendLine("</h1>");
+        sb
+        .AppendLine($"<p>avec l'email {user.Email}</p>");
+      }
+
+      sb.AppendLine("</body>");
+      sb.AppendLine("</html>");
+
+      return Results.Content(sb.ToString(), MediaTypeNames.Text.Html, Encoding.UTF8);
+    });
+
+  }
+}
